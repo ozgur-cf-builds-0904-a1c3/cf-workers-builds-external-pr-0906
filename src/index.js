58f9d84f259e5e69223939df6106b9d5b8ea92e9
ozgur-cf-builds-0904-a1c3 <ozgur.bughunter+cloudflare-builds-0904@gmail.com>
2026-09-06 20:57:16 +0300

    Owned non-main push positive control

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 export default {
   async fetch() {
-    return new Response("owned-external-pr-baseline-v1", {
+    return new Response("owned-external-pr-owner-positive-0906", {
       headers: { "content-type": "text/plain; charset=utf-8" },
     });
   },
 };
-
