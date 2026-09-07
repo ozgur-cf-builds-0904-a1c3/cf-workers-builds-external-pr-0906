1fe19197770d5fc2fb287f8ad7df66814d22a0a1
Ozgur Alp <ozgurralp@gmail.com>
2026-09-07 07:37:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-export default {
+  export default {
   async fetch() {
-    return new Response("owned-external-pr-user-pr2-0906", {
+    return new Response("owned-external-pr-user-pr3-main-included-0906", {
       headers: { "content-type": "text/plain; charset=utf-8" },
     });
   },
